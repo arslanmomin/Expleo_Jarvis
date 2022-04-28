42fa1c5f72adf013a3ca1e200b1689fdcb3365a5
Arslan Momin <73779894+arslanmomin@users.noreply.github.com>
2022-04-28 14:48:50 +0530

    updated login

--- MySession/src/test/java/jarvis/testscripts/LoginTest.java
+++ MySession/src/test/java/jarvis/testscripts/LoginTest.java
@@ -12,6 +12,7 @@ public class LoginTest extends StartBrowser {
 	  LoginLogout re= new LoginLogout();
 	  re.login(cs.getProperty("url"),cs.getProperty("email") , cs.getProperty("password"));
 	  //re.loginwithExcelData(cs.getProperty("url"));
+	   //re.loginwithExcelData(cs.getProperty("url"));
 
   }
   
