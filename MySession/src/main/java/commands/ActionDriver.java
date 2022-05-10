8a63fcc9488260549efd1d48a8ca6fe467b66d6b
12053 <12053@ESLLAP-3639.ardianet.net>
2022-05-10 18:27:40 +0530

    interface modified

--- MySession/src/main/java/commands/ActionDriver.java
+++ MySession/src/main/java/commands/ActionDriver.java
@@ -1,5 +1,19 @@
 package commands;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public interface ActionDriver {
 
+	public void openurl(WebDriver driver,String URL) ;
+	public void back(WebDriver driver);
+	public void forward(WebDriver driver);
+	public void navigate(WebDriver driver);
+	public void matchurl(WebDriver driver,String expected);
+	public void refresh(WebDriver driver);
+	
+	
+	public void click(WebDriver driver, WebElement ele, String eleName);
+	public  void JSclick(WebDriver driver, WebElement ele, String eleName);
+	
 }
