964700a0ae9908fa1d6a0e9ebc736a315b35848d
12053 <12053@ESLLAP-3639.ardianet.net>
2022-05-16 13:46:41 +0530

    formatted

--- MySession/src/main/java/configs/Base.java
+++ MySession/src/main/java/configs/Base.java
@@ -3,23 +3,12 @@ package configs;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import utilities.ConfigurationSupport;
 import utilities.ExtentManager;
-import utilities.Log;
-
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 
-import java.io.File;
-import java.lang.reflect.Method;
-import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import commands.Browser;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.log4j.xml.DOMConfigurator;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -43,20 +32,14 @@ public class Base {
 
 	public ConfigurationSupport cs = new ConfigurationSupport("config files//global.properties");
 	String browser = cs.getProperty("browser");
-	String headless=cs.getProperty("headless");
+	String headless = cs.getProperty("headless");
 
 	@BeforeTest(alwaysRun = true)
 	public void report() {
 		ExtentManager.generateReport();
-		ExtentManager.extent.setSystemInfo("browser", browser);
-		ExtentManager.extent.setSystemInfo("headless", headless);
+		ExtentManager.extent.setSystemInfo("Browser used to run : ", browser);
+		ExtentManager.extent.setSystemInfo("Is headless browser used : ", headless);
 	}
-
-	@BeforeMethod(alwaysRun = true)
-	public void methodName(Method method) {
-		
-	}
-
 
 	@BeforeSuite(alwaysRun = true)
 	public void beforeSuite() {
@@ -72,35 +55,38 @@ public class Base {
 		case "edge":
 			WebDriverManager.edgedriver().setup();
 			EdgeOptions eo = new EdgeOptions();
-			if(headless.equals("true"))
+			if (headless.equals("true"))
 				((EdgeOptions) eo).addArguments("headless");
 			eo.merge(dc);
 			driver.set(new EdgeDriver(eo));
-			getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			Browser.pageLoadTimeOut(getDriver(), 5);
 			getDriver().manage().window().maximize();
 			break;
 
 		case "firefox":
 			WebDriverManager.firefoxdriver().setup();
 			FirefoxOptions fo = new FirefoxOptions();
-			if(headless.equals("true"))
+			if (headless.equals("true"))
 				((FirefoxOptions) fo).addArguments("headless");
 			fo.merge(dc);
 			driver.set(new FirefoxDriver(fo));
-			getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			Browser.pageLoadTimeOut(getDriver(), 5);
 			getDriver().manage().window().maximize();
 			break;
 
 		case "chrome":
 			WebDriverManager.chromedriver().setup();
 			ChromeOptions co = new ChromeOptions();
-			if(headless.equals("true"))
-			((ChromeOptions) co).addArguments("headless");
+			if (headless.equals("true"))
+				((ChromeOptions) co).addArguments("headless");
 			co.merge(dc);
 			driver.set(new ChromeDriver(co));
-			getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			Browser.pageLoadTimeOut(getDriver(), 5);
 			getDriver().manage().window().maximize();
 			break;
+
+		default:
+			System.out.println("No browser selected");
 
 		}
 
@@ -108,12 +94,11 @@ public class Base {
 	}
 
 	@AfterSuite(alwaysRun = true)
-	public void afterClass() {
+	public void afterSuite() {
 
 		getDriver().close();
 		System.out.println("browser closed");
 		ExtentManager.endReport();
 	}
-
 
 }
