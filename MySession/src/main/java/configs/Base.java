4c453e7946248272d723672ab99e6e3c44ced52b
12053 <12053@ESLLAP-3639.ardianet.net>
2022-05-16 13:18:34 +0530

    headless browser completed

--- MySession/src/main/java/configs/Base.java
+++ MySession/src/main/java/configs/Base.java
@@ -12,6 +12,7 @@ import org.testng.annotations.BeforeTest;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -42,11 +43,13 @@ public class Base {
 
 	public ConfigurationSupport cs = new ConfigurationSupport("config files//global.properties");
 	String browser = cs.getProperty("browser");
+	String headless=cs.getProperty("headless");
 
 	@BeforeTest(alwaysRun = true)
 	public void report() {
 		ExtentManager.generateReport();
 		ExtentManager.extent.setSystemInfo("browser", browser);
+		ExtentManager.extent.setSystemInfo("headless", headless);
 	}
 
 	@BeforeMethod(alwaysRun = true)
@@ -54,7 +57,7 @@ public class Base {
 		
 	}
 
-	@SuppressWarnings("deprecation")
+
 	@BeforeSuite(alwaysRun = true)
 	public void beforeSuite() {
 
@@ -69,31 +72,33 @@ public class Base {
 		case "edge":
 			WebDriverManager.edgedriver().setup();
 			EdgeOptions eo = new EdgeOptions();
-			if(cs.getProperty("headless").equals("true"))
+			if(headless.equals("true"))
 				((EdgeOptions) eo).addArguments("headless");
 			eo.merge(dc);
 			driver.set(new EdgeDriver(eo));
-			getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			getDriver().manage().window().maximize();
 			break;
 
 		case "firefox":
 			WebDriverManager.firefoxdriver().setup();
 			FirefoxOptions fo = new FirefoxOptions();
+			if(headless.equals("true"))
+				((FirefoxOptions) fo).addArguments("headless");
 			fo.merge(dc);
 			driver.set(new FirefoxDriver(fo));
-			getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			getDriver().manage().window().maximize();
 			break;
 
 		case "chrome":
 			WebDriverManager.chromedriver().setup();
 			ChromeOptions co = new ChromeOptions();
-			if(cs.getProperty("headless").equals("true"))
+			if(headless.equals("true"))
 			((ChromeOptions) co).addArguments("headless");
 			co.merge(dc);
 			driver.set(new ChromeDriver(co));
-			getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			getDriver().manage().window().maximize();
 			break;
 
