5933a845145ac487b0b0f9d49353ee1f4b676f54
12053 <12053@ESLLAP-3639.ardianet.net>
2022-05-23 17:15:30 +0530

    custom methods

---
+++ MySession/src/test/java/jarvis/dataproviders/CustomMethods.java
@@ -0,0 +1,26 @@
+package jarvis.dataproviders;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+
+public class CustomMethods {
+
+	public static void accountWidget(String widgetName) {
+		// accounts-click
+		// list of web elements
+		// get text from list
+		// compare with given
+		// click on that widget
+
+	}
+
+	public static void collectionWidget(String widgetName) {
+		// collections-click
+		// list of web elements
+		// get text from list
+		// compare with given
+		// click on that widget
+
+	}
+}
