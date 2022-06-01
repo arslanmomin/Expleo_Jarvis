7e60fe8c4a91b6f658b11e71d5cdef95d8366f2e
12053 <12053@ESLLAP-3639.ardianet.net>
2022-06-01 08:42:22 +0530

    Script modified

--- MySession/src/test/java/jarvis/scripts/loginauthentications/RememberMe.java
+++ MySession/src/test/java/jarvis/scripts/loginauthentications/RememberMe.java
@@ -10,16 +10,7 @@ public class RememberMe extends Base{
 
 	Cookie cookie = new Cookie("userCrnList", "value");
 	public void addCookie() {
-		getDriver().manage().addCookie(cookie);
 		
-		Cookie cookieValue = getDriver().manage().getCookieNamed("userCrnList");
-        System.out.println(cookieValue.getValue());
-        
-		Set<Cookie> cookieList = getDriver().manage().getCookies();
-		for (Cookie newCookie : cookieList) {
-			System.out.println(newCookie);
-		}
-		getDriver().manage().deleteCookieNamed("userCrnList");
 		
 	}
 	
